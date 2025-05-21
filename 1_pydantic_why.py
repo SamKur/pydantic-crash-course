a496adacd4ddfd2258873215aeae0f562eba9b23
Susamay Kumbhakar <susamay.sk@gmail.com>
2025-05-21 20:19:13 +0530

    initial tweaks

--- 1_pydantic_why.py
+++ 1_pydantic_why.py
@@ -1,28 +1,109 @@
-from pydantic import BaseModel, EmailStr, AnyUrl, Field
-from typing import List, Dict, Optional, Annotated
+def insert_patient_data1(name, age):
+    print(name)
+    print(age)
+    print('inserted into db')
 
+
+insert_patient_data1('susamay', 20)         # Correct data pass
+insert_patient_data1('susamay', 'twenty')   # Incorrect data pass due to dynamic typing
+
+print("=== NOW USING BUIT-IN PYTHON TYPE HINTING ===")
+
+def insert_patient_data2(name, age):
+    print(name)
+    print(age)
+    print('inserted into db with type hinting')
+
+
+insert_patient_data2('susamay', 'twenty')   # Type hinting does not enforce type checking at runtime
+
+print("=== NOW USING MANUAL TYPE CHECKING ===")
+
+def insert_patient_data3(name, age):
+    if not isinstance(name, str):           # or, if type(name) is not str:
+        raise TypeError('Name must be a string')
+    if not isinstance(age, int):
+        raise TypeError('Age must be an integer')
+    if age < 0 or age > 120:                # data validation
+        raise ValueError('Age must be between 0 and 120')
+    print(name)
+    print(age)
+    print('inserted into db with manual type checking // Not Scalable')
+
+# insert_patient_data3('susamay', 'twenty')  # Throws TypeError
+insert_patient_data3('susamay', 20)          # Correct data pass -> we need to write this checks for every function
+
+
+print("=== INSTEAD USE PYDANTIC FOR TYPE AND DATA VALIDATION ===")
+print("=== SIMPLE USAGE OF PYDANTIC ===")
+
+from pydantic import BaseModel
+from typing import List, Dict, Optional
+
+# Step1: Pydantic Model
 class Patient(BaseModel):
+    name: str
+    age: int
+    weight: float
+    married: bool = False                   # default value
+    allergies: Optional[List[str]] = None   # Optional but having default value
+    contact_details: Dict[str, str]
 
+def update_patient_data(patient: Patient):
+    print(patient.name)
+    print(patient.age)
+    # print(patient.allergies)              # Let's say we're not using everything for now
+    # print(patient.married)
+    print('updated')
+
+patient_info = {                            # Step2: Define input data (as a plain dictionary)
+    'name': 'Susamay',
+    'age': 26,
+    'weight': 57.5,    
+    'married': False,
+    'allergies': ['pollen', 'dust'],
+    'contact_details': {'phone': '2353462'}
+}
+
+patient1 = Patient(**patient_info)          # Step 3: Validate and parse the data (create a model instance)
+update_patient_data(patient1)               # Pass the instance to the function
+
+
+print("=== PYDANTIC WITH MORE TYPE AND DATA VALIDATION ===")
+
+
+from typing import Annotated, Optional, List, Dict
+from pydantic import BaseModel, Field, EmailStr, AnyUrl
+
+# Step1: Pydantic Model
+class Patient(BaseModel):
+    # name: str = Field(max_length=50)
     name: Annotated[str, Field(max_length=50, title='Name of the patient', description='Give the name of the patient in less than 50 chars', examples=['Nitish', 'Amit'])]
     email: EmailStr
     linkedin_url: AnyUrl
-    age: int = Field(gt=0, lt=120)
+    age: int = Field(gt=0, lt=120)  # DATA VALIDATION - age must be between 0 and 120
     weight: Annotated[float, Field(gt=0, strict=True)]
-    married: Annotated[bool, Field(default=None, description='Is the patient married or not')]
-    allergies: Annotated[Optional[List[str]], Field(default=None, max_length=5)]
+    married: Annotated[Optional[bool], Field(default=None, description='Is the patient married or not')]
+    allergies: Annotated[Optional[List[str]], Field(default=None, max_items=5)]
     contact_details: Dict[str, str]
-
-
+    
 def update_patient_data(patient: Patient):
-
     print(patient.name)
     print(patient.age)
     print(patient.allergies)
     print(patient.married)
     print('updated')
 
-patient_info = {'name':'nitish', 'email':'abc@gmail.com', 'linkedin_url':'http://linkedin.com/1322', 'age': '30', 'weight': 75.2,'contact_details':{'phone':'2353462'}}
+# Step2: Define input data (as a plain dictionary)
+patient_info = {
+    'name': 'SusamAY',
+    'email': 'sk@gmail.com',
+    'linkedin_url': 'http://linkedin.com/in/susamay-sk',
+    'age': 26,  # pass as int
+    'weight': 55.2,
+    'contact_details': {'phone': '4209211'}
+}
 
+# Step3: Validate and parse the data (create a model instance)
 patient1 = Patient(**patient_info)
-
 update_patient_data(patient1)
